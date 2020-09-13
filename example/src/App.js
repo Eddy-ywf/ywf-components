ae87ef6961d72862c764fea7d26530215768e308
Eddy-ywf <eddyenclave@gmail.com>
2020-09-13 11:11:46 +0700

    update color button

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { ExampleComponent } from 'ywf-components'
+import { Button } from 'ywf-components'
 import 'ywf-components/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Button outline color='yoodoo-primary'>
+      Yoodoo Button 123
+      </Button>
+
+     <Button color="ywf-primary">Yoodoo Button</Button>
+    </>
+)
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
 import './assets/scss/styles.module.scss';
 
 import { Button } from './components';
 
-export const ExampleComponent = ({ text }) => {
-  return (
-    <Button outline color='yoodoo-primary'>
-      Yoodoo Button
-    </Button>
-  );
-};
+export { Button };
